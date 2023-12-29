0f0a92ccf7c0f190d0968354208e734bf090326d
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-29 09:25:29 -0300

    21° commit - tdd controller: Consegue listar um database especifico e retornar status 200? GREEN

--- app/controllers/api/v1/databases_controller.rb
+++ app/controllers/api/v1/databases_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::DatabasesController < ApplicationController
 
-  #before_action :set_database, only: %i[] # show update destroy
+  before_action :set_database, only: %i[show] # show update destroy
 
   def index
     @databases = Database.all 
     render json: @databases
+  end
+
+  def show
+    render json: @database
   end
 
 private
