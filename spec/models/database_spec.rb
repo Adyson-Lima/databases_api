1782477c977853c15181d79cfcf393c4da0d3152
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-29 08:50:33 -0300

    10° commit - tdd model: name consegue ser preenchido? RED

---
+++ spec/models/database_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Database, type: :model do
+
+  before{@database = Database.new}
+
+  describe 'Testes de preenchimento do model Database' do
+
+    it 'name consegue ser preenchido?' do
+      @database.name = ''
+      expect(@database.name).to eq('postgresql')
+    end
+
+  end
+
+end
