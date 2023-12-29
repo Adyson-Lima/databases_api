d5febd345f7d187e0d7eded5e03c87788e312915
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-29 09:49:23 -0300

    28° commit - seeds.rb configurado

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'criando databases...'
+10.times do
+  Database.create(name: ['sqlite', 'mariadb', 'postgresql'].sample, community: 'comunidade própria')
+end
+puts 'databases criados com sucesso!'
